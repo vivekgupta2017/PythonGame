a2446c806e82b7658fa5324a4a96e7d0935b8a6e
Vivek Gupta <vivekgupta2017@gmail.com>
2020-07-28 09:37:05 +0530

    Add files via upload

---
+++ project/Project/game_config.py
@@ -0,0 +1,12 @@
+import os
+
+IMG_SIZ=128
+SCR_SIZ=512
+NUM_TILES_SIDE=4
+NUM_TILES_TOTAL=16
+MARGIN = 4
+
+ASSET_DIR = 'assets'
+ASSET_FILES = [x for x in os.listdir(ASSET_DIR) if x[-3:].lower() == 'png']
+
+assert len(ASSET_FILES) == 8
